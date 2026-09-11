c757f736070029e32cc739c0f4b1177cdbc2e7d5
Vicente Javier Viera Guizar <vicentejvg@outlook.com>
2026-09-10 19:01:38 -0600

    Add imgui and rlimgui submodules; update project version to 0.0.3

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <config.hpp>
 
-int main(int argc, char* argv[]){
-    std::cout<<project_name<<std::endl;
-    std::cout<<project_version<<std::endl;
-    std::cout<<"C++ Standard: "<<__cplusplus<<std::endl;
+#include <imgui.h>
+#include <raylib.h>
+#include <rlImGui.h>
+
+int main(int argc, char* argv[]) {
+    std::cout << project_name << std::endl;
+    std::cout << project_version << std::endl;
+    std::cout << "C++ Standard: " << __cplusplus << std::endl;
 
     return 0;
 }
